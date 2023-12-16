062d1f203ac4c9583e2bd489f4d8a1d74638e502
Muhammed Fahad <muhammedfahadh@outlook.com>
2023-12-16 14:55:58 +0530

    Updated App Page and Next Config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import { fuels, yearsOfProduction } from '@/constants';
-import { HomeProps } from '@/types/index'
+import { HomeProps } from '@/types'
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
